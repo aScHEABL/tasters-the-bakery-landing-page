cecb78cb48e7d3d118395528420c7759479fdbca
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-23 10:37:38 +0800

    WIP: AppContext.js #0

--- src/App.js
+++ src/App.js
@@ -8,33 +8,21 @@ import {
   Code,
   Grid,
   theme,
+  Container,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { AppContextProvider } from './AppContext';
+import FirstPage from './components/FirstPage';
 
 function App() {
+  // <ColorModeSwitcher justifySelf="flex-end" />
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <AppContextProvider>
+        <Container>
+          <FirstPage />
+        </Container>
+      </AppContextProvider>
     </ChakraProvider>
   );
 }
---
+++ src/AppContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useReducer } from "react";
+
+const AppContext = createContext();
+
+const initialState = {
+    language: "zh-tw",
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'switch_language':
+            return {
+                ...state,
+                language: state.language === "zh-tw" ? "en-us" : "zh-tw",
+            }
+        default:
+            return state;
+    }
+}
+
+function AppContextProvider({ children }) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <AppContext.Provider value={{ state, dispatch }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export { AppContext, AppContextProvider }
