125b6f1ab3cc64602b251705c58c8909bc1e9a72
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-23 14:00:52 +0800

    init: FirstPage.js

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Flex, Button } from "@chakra-ui/react";
-import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { HiLanguage } from "react-icons/hi";
+import { MdLanguage } from "react-icons/md";
+import { AppContext } from "../AppContext"; 
+import zhTwTranslation from "../language/zh-tw-lang.json";
+import enUsTranslation from "../language/en-us-lang.json";
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
 
+{/* <ColorModeSwitcher justifySelf="flex-end" /> */}
 function FirstPage() {
+    const { state, dispatch } = useContext(AppContext);
+    const displayLanguage = state.language === "zh-tw" ? zhTwTranslation : enUsTranslation
+
+    const handleLanguageSwitch = () => {
+        dispatch({ type: 'SWITCH_LANGUAGE' });
+    }
+    
     return (
-        <Flex as='nav'>
-            
+        <Flex as='nav' justify='flex-end'>
+            {state.language === "zh-tw" ? 
+                <Button size='lg' leftIcon={<MdLanguage />} colorScheme='gray' variant='ghost' onClick={handleLanguageSwitch}>
+                    English
+                </Button>
+                :
+                <Button size='lg' leftIcon={<MdLanguage />} colorScheme='gray' variant='ghost'  onClick={handleLanguageSwitch}>
+                    中文
+                </Button>
+            }
+            <Button size='lg' colorScheme='gray' variant='ghost'>{displayLanguage.nav_intro_btn}</Button>
+            <Button size='lg' colorScheme='gray' variant='ghost'>{displayLanguage.nav_product_btn}</Button>
+            <Button size='lg' colorScheme='gray' variant='ghost'>{displayLanguage.nav_gallery_btn}</Button>
+            <Button size='lg' colorScheme='gray' variant='ghost'>{displayLanguage.nav_order_btn}</Button>
+            <Button size='lg' colorScheme='gray' variant='ghost'>{displayLanguage.nav_contact_btn}</Button>
         </Flex>
     )
 }
