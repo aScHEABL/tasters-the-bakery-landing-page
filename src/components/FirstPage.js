4286c87a8ba06d5c8b5017caac8e73a295253097
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-05-23 10:36:55 +0800

    init: FirstPage.js

---
+++ src/components/FirstPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Flex, Button } from "@chakra-ui/react";
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { HiLanguage } from "react-icons/hi";
+
+function FirstPage() {
+    return (
+        <Flex as='nav'>
+            
+        </Flex>
+    )
+}
+
+export default FirstPage;
